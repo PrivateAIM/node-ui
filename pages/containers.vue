c08c7a8179d2d86353d8945cbd6ead908554065b
Bruce Schultz <bschultz013@gmail.com>
2024-06-13 08:16:04 +0200

    feat(rbac): try out manually disabling components if not logged in

--- pages/containers.vue
+++ pages/containers.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
+<script setup>
+// const { loggedIn } = useOidcAuth();
+</script>
 
 <template>
+  <!--  <div v-if="loggedIn">-->
   <ContainerTable />
+  <!--  </div>-->
 </template>
 
 <style scoped lang="scss"></style>
