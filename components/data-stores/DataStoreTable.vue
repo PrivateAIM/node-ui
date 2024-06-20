56a1013856e337980f39775ea06d84c5efeda21d
Bruce Schultz <bschultz013@gmail.com>
2024-06-20 14:21:58 +0200

    feat(ds): add confirmation box when deleting data store

--- components/data-stores/DataStoreTable.vue
+++ components/data-stores/DataStoreTable.vue
@@ -2,8 +2,11 @@
 import { deleteDataStore, getDataStores } from "~/composables/useAPIFetch";
 import type { Route } from "~/services/Api";
 import { parseUnixTimestamp } from "~/utils/parse-unix-timestamp";
+import { useConfirm } from "primevue/useconfirm";
 
 const dataStores = ref();
+
+const confirm = useConfirm();
 
 onMounted(() => {
   nextTick(async () => {
@@ -16,10 +19,27 @@ onMounted(() => {
   });
 });
 
-function onSubmitDeleteDataStore(dsName: string) {
+function onConfirmDeleteDataStore(dsName: string) {
   deleteDataStore(dsName);
   window.location.reload();
 }
+
+const confirmDelete = (event, dsName: string) => {
+  confirm.require({
+    target: event.currentTarget,
+    group: "templating",
+    message: "Are you sure you want to delete this data store?",
+    icon: "pi pi-exclamation-circle",
+    acceptIcon: "pi pi-check",
+    acceptLabel: "Confirm",
+    rejectIcon: "pi pi-times",
+    rejectLabel: "Cancel",
+    accept: () => {
+      onConfirmDeleteDataStore(dsName);
+    },
+    reject: () => {},
+  });
+};
 console.log(dataStores);
 </script>
 
@@ -42,11 +62,25 @@ console.log(dataStores);
       <Column field="updated_at" header="Last Updated"></Column>
       <Column field="name" header="Delete?" :exportable="false">
         <template #body="slotProps">
+          <Toast />
+          <ConfirmPopup group="templating">
+            <template #message="slotProps">
+              <div
+                class="flex flex-col items-center w-full gap-4 border-b border-surface-200 dark:border-surface-700 p-4 mb-4 pb-0"
+              >
+                <i
+                  :class="slotProps.message.icon"
+                  class="text-6xl text-primary-500"
+                ></i>
+                <p>{{ slotProps.message.message }}</p>
+              </div>
+            </template>
+          </ConfirmPopup>
           <Button
             icon="pi pi-trash"
             aria-label="Delete"
             severity="danger"
-            @click="onSubmitDeleteDataStore(slotProps.data.name)"
+            @click="confirmDelete($event, slotProps.data.name)"
           />
         </template>
       </Column>
