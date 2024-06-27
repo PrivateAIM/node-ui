2257460a4b9e29a136084995294573d9677cfc87
Bruce Schultz <bschultz013@gmail.com>
2024-06-27 09:38:38 +0200

    style(login): add picture to home and move elements

--- components/KeycloakAuth.vue
+++ components/KeycloakAuth.vue
@@ -3,12 +3,13 @@ const { loggedIn, user, login, logout } = useOidcAuth();
 </script>
 
 <template>
-  <div v-if="loggedIn">
-    <h3>Welcome {{ user.providerInfo.preferred_username }}!</h3>
-    <button @click="logout()">Logout</button>
-  </div>
-  <div v-else>
-    <h1>Not logged in</h1>
-    <button @click="login()">Login with Keycloak</button>
+  <div class="keycloakLoginButton">
+    <div v-if="loggedIn">
+      <h3>Welcome {{ user.providerInfo.preferred_username }}!</h3>
+      <button @click="logout()">Logout</button>
+    </div>
+    <div v-else>
+      <button @click="login()">Login with Keycloak</button>
+    </div>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,35 @@ import KeycloakAuth from "~/components/KeycloakAuth.vue";
 </script>
 
 <template>
-  <div>
-    <h1>Node UI Home Page</h1>
+  <div id="home" class="row">
+    <div class="col-6 welcome">
+      <h1>FLAME Node UI</h1>
+      <KeycloakAuth />
+    </div>
+    <div class="col-6">
+      <img
+        src="../assets/hospital_network.jpg"
+        alt="Hospital Network"
+        class="pimg networkImg"
+      />
+    </div>
   </div>
-  <div><KeycloakAuth /></div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+#home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.pimg {
+  height: 650px;
+  float: right;
+  display: flex;
+}
+
+.row {
+  display: flex;
+}
+</style>
