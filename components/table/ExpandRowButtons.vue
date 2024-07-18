caff69d8d91ffe5ae67f50ac79c0044f9b139cc7
Bruce Schultz <bschultz013@gmail.com>
2024-07-18 13:52:55 +0200

    refactor(tables): create component for expanding rows in tables

---
+++ components/table/ExpandRowButtons.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const rows = ref();
+const expandedRows = ref({});
+
+const expandAll = () => {
+  expandedRows.value = rows.value.reduce(
+    (accordion: boolean, row) =>
+      // eslint-disable-next-line no-constant-binary-expression
+      (accordion[row.id] = true) && accordion,
+    {},
+  );
+};
+
+const collapseAll = () => {
+  expandedRows.value = {};
+};
+</script>
+
+<template>
+  <div class="flex flex-wrap justify-end gap-2 expandButtons">
+    <Button text icon="pi pi-plus" label="Expand All" @click="expandAll" />
+    <Button text icon="pi pi-minus" label="Collapse All" @click="collapseAll" />
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
