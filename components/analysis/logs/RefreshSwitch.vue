cf2657344f47fd40f1ba73a0a8bd4cf3dd99bbc3
Bruce Schultz <bschultz013@gmail.com>
2024-09-10 11:16:47 +0200

    feat(logs): create working log update switch

---
+++ components/analysis/logs/RefreshSwitch.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const periodicRefresh = ref(false);
+</script>
+
+<template>
+  <div class="justify-content-center log-toggle-switch">
+    <span style="font-size: 12px; padding-right: 0.5em">
+      Refresh periodically
+    </span>
+    <InputSwitch
+      v-model="periodicRefresh"
+      v-tooltip.top="'Refresh logs every 5 seconds'"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
