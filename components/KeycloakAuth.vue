29a6156671ae62957095cd1ac2b66b236dabe36f
Bruce Schultz <bschultz013@gmail.com>
2024-10-16 12:27:15 +0200

    chore(config): simplify nuxt config

--- components/KeycloakAuth.vue
+++ components/KeycloakAuth.vue
@@ -1,18 +1,12 @@
 <script setup lang="ts">
 const { loggedIn, user, login, logout } = useOidcAuth();
-
-async function customLogout() {
-  await logout();
-}
 </script>
 
 <template>
   <div class="keycloakLoginButton">
     <div v-if="loggedIn">
       <h3>Welcome {{ user.userInfo?.preferred_username || "Friend" }}!</h3>
-      <Button @click="customLogout()" severity="warning" outlined
-        >Logout</Button
-      >
+      <Button @click="logout()" severity="warning" outlined>Logout</Button>
     </div>
     <div v-else>
       <Button @click="login()" severity="success" outlined
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,35 +29,16 @@ export default defineNuxtConfig({
       keycloak: {
         audience: "account",
         userNameClaim: "preferred_username",
-        clientId:
-          process.env.NUXT_OIDC_PROVIDERS_KEYCLOAK_CLIENT_ID || "node-ui",
-        clientSecret: process.env
-          .NUXT_OIDC_PROVIDERS_KEYCLOAK_CLIENT_SECRET as string,
-        redirectUri:
-          process.env.NUXT_OIDC_PROVIDERS_KEYCLOAK_REDIRECT_URI ||
-          process.env.NUXT_PUBLIC_BASE_URL + "/auth/keycloak/callback",
-        authorizationUrl:
-          process.env.NUXT_OIDC_PROVIDERS_KEYCLOAK_AUTHORIZATION_URL ||
-          process.env.KEYCLOAK_LOGIN_URL +
-            "/realms/flame/protocol/openid-connect/auth",
-        tokenUrl:
-          process.env.NUXT_OIDC_PROVIDERS_KEYCLOAK_TOKEN_URL ||
-          process.env.KEYCLOAK_SERVICE_URL +
-            "/realms/flame/protocol/openid-connect/auth",
-        userinfoUrl:
-          process.env.NUXT_OIDC_PROVIDERS_KEYCLOAK_USERINFO_URL ||
-          process.env.KEYCLOAK_SERVICE_URL +
-            "/realms/flame/protocol/openid-connect/auth",
-        openIdConfiguration:
-          process.env.NUXT_OIDC_PROVIDERS_KEYCLOAK_OPEN_ID_CONFIGURATION ||
-          process.env.KEYCLOAK_SERVICE_URL +
-            "/realms/flame/.well-known/openid-configuration",
-        logoutUrl:
-          process.env.NUXT_OIDC_PROVIDERS_KEYCLOAK_LOGOUT_URL ||
-          process.env.KEYCLOAK_SERVICE_URL +
-            "/realms/flame/protocol/openid-connect/logout",
-        logoutRedirectUri: process.env.NUXT_PUBLIC_BASE_URL,
-        exposeAccessToken: true,
+        clientId: "",
+        clientSecret: "",
+        redirectUri: "",
+        authorizationUrl: "",
+        tokenUrl: "",
+        userinfoUrl: "",
+        openIdConfiguration: "",
+        logoutUrl: "",
+        logoutRedirectUri: "",
+        exposeAccessToken: false,
         pkce: false,
       },
     },
