52d61a16ed545ecc64c6631cc983ce7a578b42e6
Bruce Schultz <bschultz013@gmail.com>
2024-10-17 10:42:26 +0200

    revert(auth): hacky fix build_image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,6 +38,7 @@ export default defineNuxtConfig({
         openIdConfiguration: "",
         logoutUrl: "",
         logoutRedirectUri: "",
+        validateAccessToken: false,
         exposeAccessToken: false,
         pkce: false,
       },
