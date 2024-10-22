135b51f5985e950c986ad7a8d7f2dfc9ec6b4c88
Bruce Schultz <bschultz013@gmail.com>
2024-10-22 10:55:34 +0200

    refactor(project): disable project overview page and replace with project nodes

--- pages/proposals.vue
+++ pages/proposals.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import ProposalTable from "~/components/projects/ProjectProposalTable.vue";
+import ProjectProposalTable from "~/components/projects/ProjectProposalTable.vue";
 
 definePageMeta({
   middleware: ["auth"],
@@ -7,7 +7,7 @@ definePageMeta({
 </script>
 
 <template>
-  <ProposalTable />
+  <ProjectProposalTable />
 </template>
 
 <style scoped lang="scss"></style>
