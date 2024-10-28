a251ed49f68abb169a75dcbe6a1dd0a1c7f9034b
Bruce Schultz <bschultz013@gmail.com>
2024-10-28 09:48:07 +0100

    build(config): disable userinfo config param

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,7 +34,7 @@ export default defineNuxtConfig({
         redirectUri: "",
         authorizationUrl: "",
         tokenUrl: "",
-        userinfoUrl: "",
+        // userinfoUrl: "",
         openIdConfiguration: "",
         logoutUrl: "",
         logoutRedirectUri: "",
