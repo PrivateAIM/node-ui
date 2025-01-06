3608604c5f64fa999839889970c0cf800375ad83
Bruce Schultz <bschultz013@gmail.com>
2025-01-06 11:42:13 +0100

    feat(ds): add text to help fields

--- components/data-stores/managers/DataStoreHelpBox.vue
+++ components/data-stores/managers/DataStoreHelpBox.vue
@@ -9,12 +9,134 @@ const props = defineProps({
 <template>
   <div class="data-store-help">
     <Fieldset :legend="props.helpField" :toggleable="false">
-      <p v-if="props.helpField === HelpTextField.Methods">Methods</p>
-      <p v-else-if="props.helpField === HelpTextField.Path">Path</p>
-      <p v-else-if="props.helpField === HelpTextField.Port">Port</p>
-      <p v-else-if="props.helpField === HelpTextField.Protocol">Protocol</p>
-      <p v-else-if="props.helpField === HelpTextField.Server">Server</p>
-      <p v-else-if="props.helpField === HelpTextField.Type">Type</p>
+      <div
+        class="methods-help-text"
+        v-if="props.helpField === HelpTextField.Methods"
+      >
+        <h3>Request Methods</h3>
+        <p>
+          How the data is accessed can be tightly controlled by defining which
+          request methods are allowed for a given project. For a project to be
+          able to read the data, the <code>GET</code> method must be enabled
+          (and is by default).
+        </p>
+        <p>
+          Additional request methods can be added to this list which would allow
+          users of the project to modify (<code>PUT</code>) the data, add
+          (<code>POST</code>) new data, or remove (<code>DELETE</code>) data in
+          the server. The ability to modify the list of allowed request methods
+          is disabled by default, though admins can enable this field, but
+          should understand the risks of allowing users to directly modify the
+          data on their institution's servers.
+        </p>
+      </div>
+      <div
+        class="methods-help-text"
+        v-else-if="props.helpField === HelpTextField.Path"
+      >
+        <h3>Directory Path</h3>
+        <p>
+          Here, the admin must provide the absolute file path of the directory
+          (folder) which contains the relevant data to be shared with the
+          project. The path should be the same as defined on the server/host
+          provided in the <i>Server</i> field.
+        </p>
+        <p>
+          For Unix based systems, the directory path should start with a forward
+          slash "/", while a Windows OS will use a drive letter with a colon and
+          2 back slashes (e.g. H:\\).
+        </p>
+      </div>
+      <div
+        class="methods-help-text"
+        v-else-if="props.helpField === HelpTextField.Port"
+      >
+        <h3>Server Port</h3>
+        <p>
+          In order to maintain security, firewalls are used to prevent
+          unauthorized access to professional and personal computers/servers. To
+          gain access to services or data on a system with a firewall, a
+          <i>port</i> must be opened by the technical administrator. These ports
+          have numerical identifiers and your institution should have a specific
+          port opened for the server containing the data to be shared.
+        </p>
+        <p>
+          Please provide that port number here so that the FLAME Node Service is
+          able to access the data. If you are unsure or do not know which port
+          should be used, please contact your IT department or relevant
+          administrator.
+        </p>
+      </div>
+      <div
+        class="methods-help-text"
+        v-else-if="props.helpField === HelpTextField.Protocol"
+      >
+        <h3>Communication Protocol</h3>
+        <p>
+          Multiple protocols exist for transferring files between computers and
+          for communication, the most common being HTTP. To improve the security
+          of the transferred data, <b>Transport Layer Security</b> (TLS) was
+          developed for protocols as an encryption method and is now very
+          commonly used (HTTPS is simply HTTP over TLS). The FLAME Node software
+          needs to know the protocol used, and whether or not it is secured
+          using TLS.
+        </p>
+        <ul>
+          <li>
+            HTTP/HTTPS - Hypertext Transfer Protocol. Data is generally sent
+            using port 80 (HTTP) or port 443 (HTTPS)
+          </li>
+          <li>WS/WSS - Websocket on HTTP (WS) or on HTTPS (WSS)</li>
+          <li>gRPC/gRPCS - Google remote procedural calls</li>
+          <li>TCP/TLS - Transmission Control Protocol</li>
+          <li>
+            TCP Passthrough - a TLS proxy request that doesn't terminate (a SNI
+            proxy)
+          </li>
+        </ul>
+        <p>
+          For additional information, please visit
+          <a
+            href="https://docs.konghq.com/gateway/latest/how-kong-works/routing-traffic/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            the Kong documentation.
+          </a>
+        </p>
+        <p>
+          Should your institution use a different protocol other than those
+          listed below, please contact the Node Dev Team for help resolving
+          this.
+        </p>
+      </div>
+      <div
+        class="methods-help-text"
+        v-else-if="props.helpField === HelpTextField.Server"
+      >
+        <h3>Server Host</h3>
+        <p>
+          This field is for providing the <b>hostname</b> or
+          <b>IP address</b> of the data's server. Please take care that this is
+          reachable from outside of the network.
+        </p>
+      </div>
+      <div
+        class="methods-help-text"
+        v-else-if="props.helpField === HelpTextField.Type"
+      >
+        <h3>Data Store Type</h3>
+        <p>
+          Depending on how the data is made available, the FLAME Node software
+          will have to setup specific plugins and features. It is important to
+          indicate here in what type of instance the data is stored.
+        </p>
+        <p>
+          Should your institution use a different method of storage other than
+          those listed, please contact the Node Dev Team for help resolving
+          this.
+        </p>
+      </div>
       <p v-else>Foobar</p>
     </Fieldset>
   </div>
