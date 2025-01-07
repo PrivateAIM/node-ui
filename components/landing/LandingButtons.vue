d2589dd685d30188b2def6a0c639ce1624d21461
Bruce Schultz <bschultz013@gmail.com>
2025-01-07 13:50:14 +0100

    style(landing): improve btn css

--- components/landing/LandingButtons.vue
+++ components/landing/LandingButtons.vue
@@ -24,25 +24,19 @@ const { loggedIn } = useOidcAuth();
       <h2 style="text-align: center">What would you like to do today?</h2>
       <div class="get-started-btns">
         <a class="get-started-btn" href="/projects">
-          <i
-            class="pi pi-objects-column get-started-btn-icon"
-            style="font-size: 3em"
-          />
+          <i class="pi pi-objects-column get-started-btn-icon" />
           <p>Review projects and their statuses</p>
         </a>
         <a class="get-started-btn" href="/analyses">
-          <i
-            class="pi pi-lightbulb get-started-btn-icon"
-            style="font-size: 3em"
-          />
+          <i class="pi pi-lightbulb get-started-btn-icon" />
           <p>Inspect individual analyses</p>
         </a>
         <a class="get-started-btn" href="/data-stores/create">
-          <i class="pi pi-hammer get-started-btn-icon" style="font-size: 3em" />
+          <i class="pi pi-hammer get-started-btn-icon" />
           <p>Create a data store for a project</p>
         </a>
         <a class="get-started-btn" href="/data-stores">
-          <i class="pi pi-wrench get-started-btn-icon" style="font-size: 3em" />
+          <i class="pi pi-wrench get-started-btn-icon" />
           <p>Manage the active data stores</p>
         </a>
       </div>
@@ -69,27 +63,35 @@ const { loggedIn } = useOidcAuth();
   display: flex;
   flex-direction: row;
   justify-content: safe center;
-  height: 10em;
+  margin: auto;
 }
 
 .get-started-btn {
-  width: 10em;
-  border: solid 1px gray;
+  width: 40%;
+  border: solid 2px rgba(gray, 0.2);
   background: none;
   text-decoration: none;
-  display: flex;
-  flex-direction: column;
   text-align: center;
-  align-items: center;
   color: deepskyblue;
   padding: 0.4em;
 }
 
-.get-started-btn-icon {
-  margin: 0.3em;
-}
-
 .get-started-btn:hover {
   border-bottom: solid 2px yellow;
+  margin-bottom: -2px;
+  background-color: black;
+}
+
+.get-started-btn:not(:last-child) {
+  border-right: solid 1px rgba(gray, 0.2);
+}
+
+.get-started-btn:not(:first-child) {
+  border-left: solid 1px rgba(gray, 0.2);
+}
+
+.get-started-btn-icon {
+  margin: 0.3em;
+  font-size: 3em;
 }
 </style>
