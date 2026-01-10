cc8255447105408ccec07e9e51623301ef8d8586
Bruce Schultz <bschultz013@gmail.com>
2026-01-10 11:06:19 +0100

    style: new landing page png

--- pages/index.vue
+++ pages/index.vue
@@ -22,11 +22,13 @@ definePageMeta({
           maintaining full control of who has access to your data.
         </p>
       </div>
-      <div class="landing-btns"><LandingButtons /></div>
+      <div class="landing-btns">
+        <LandingButtons />
+      </div>
     </div>
     <div class="col-6 landing-img">
       <img
-        src="@/assets/img/hospital_network.jpg"
+        src="@/assets/img/node_network.png"
         alt="Hospital Network"
         class="pimg networkImg"
       />
