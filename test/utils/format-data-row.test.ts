b1e45ccb44b68b89a1a48cb61829ce8c503c5cc8
Bruce Schultz <bschultz013@gmail.com>
2026-03-05 13:44:26 +0100

    test(tz): pin TZ in vitest config

--- test/utils/format-data-row.test.ts
+++ test/utils/format-data-row.test.ts
@@ -5,7 +5,7 @@ import { fakeAnalysisNodes } from "@/test/components/analysis/constants";
 import type { ModifiedAnalysisNode } from "~/services/modifiedApiInterfaces";
 
 test("Format Unix timestamp", () => {
-  const fakeAnalysisNode = fakeAnalysisNodes[0];
+  const fakeAnalysisNode = fakeAnalysisNodes[0]!;
 
   expect(fakeAnalysisNode.created_at).toBe("2025-01-28T14:58:12Z");
   expect(fakeAnalysisNode.updated_at).toBe("2025-03-18T09:11:14Z");
@@ -34,8 +34,9 @@ test("Format Unix timestamp", () => {
 });
 
 test("Format returned analysis data", () => {
-  expect(fakeAnalysisNodes[0].created_at).toBe("2025-01-28T14:58:12Z");
-  expect(fakeAnalysisNodes[0].updated_at).toBe("2025-03-18T09:11:14Z");
+  const baseNode = fakeAnalysisNodes[0]!;
+  expect(baseNode.created_at).toBe("2025-01-28T14:58:12Z");
+  expect(baseNode.updated_at).toBe("2025-03-18T09:11:14Z");
 
   const updatedRows = formatDataRow(
     fakeAnalysisNodes,
@@ -43,14 +44,16 @@ test("Format returned analysis data", () => {
     [],
   ) as ModifiedAnalysisNode[];
 
-  expect(updatedRows[0].created_at).toStrictEqual({
+  const row = updatedRows[0]!;
+
+  expect(row.created_at).toStrictEqual({
     short: "28.01.2025",
     long: "28. Jan. 2025, 15:58",
     date: new Date("2025-01-28T14:58:12.000Z"),
     timestamp: 1738076292,
   });
 
-  expect(updatedRows[0].updated_at).toStrictEqual({
+  expect(row.updated_at).toStrictEqual({
     short: "18.03.2025",
     long: "18. März 2025, 10:11",
     date: new Date("2025-03-18T09:11:14.000Z"),
