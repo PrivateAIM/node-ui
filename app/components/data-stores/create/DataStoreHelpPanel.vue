5f3fbdbdfbebe90c6ff9d08ace0e5b6f3303b70f
Bruce Schultz <bschultz013@gmail.com>
2026-04-02 08:02:23 +0200

    feat(data-stores): add DataStoreHelpPanel sub-component

---
+++ app/components/data-stores/create/DataStoreHelpPanel.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import Panel from "primevue/panel";
+
+defineProps<{ header: string }>();
+const emit = defineEmits(["closeHelp"]);
+</script>
+
+<template>
+  <div class="methods-help-text">
+    <Panel :header="header">
+      <slot />
+      <template #icons>
+        <Button
+          class="p-panel-header-icon p-link mr-2 help-box-close-btn"
+          icon="pi pi-times"
+          severity="contrast"
+          v-tooltip.top="'Close help box'"
+          @click="emit('closeHelp')"
+        />
+      </template>
+    </Panel>
+  </div>
+</template>
